8e834daea3ef3a494d7612ddb041c8a0ccc7a175
MALAK0244 <malak.hussein0244@icloud.com>
2025-03-08 22:52:50 +0200

    language

--- lib/main.dart
+++ lib/main.dart
@@ -60,8 +60,9 @@ class MyApp extends StatelessWidget {
         MovieDetails.routeName: (_) => MovieDetails(),
         ProfileUpdate.routeName: (_) => ProfileUpdate(),
       },
-      initialRoute: HomeScreen.routeName,
+      initialRoute: LoginScreen.routeName,
       theme: AppTheme.generalTheme,
     );
+
   }
 }
